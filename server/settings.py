140937bab227fdaa923bd59c698b3107ac0d82f0
Loai Esam <114489561+LoaiEsam37@users.noreply.github.com>
2023-06-21 22:32:44 +0300

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h8jl^*0p1_zt_cx-2-f*7l%o33as@z8p909$f%ajpr(w7$*v0t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-simple-jwt-auth-production.up.railway.app"]
 
 
 # Application definition
